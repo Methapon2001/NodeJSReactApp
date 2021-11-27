ce203f03993ae6b6ca3b071455ab9fcbb57ab1b3
Methapon2001 <dark.methapon13@gmail.com>
2021-11-27 17:07:33 +0700

    Added edit data system

--- client/src/components/ViewData.js
+++ client/src/components/ViewData.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Modal from 'react-awesome-modal';
 import axios from 'axios';
 
 export default class ViewData extends React.Component {
@@ -6,7 +7,13 @@ export default class ViewData extends React.Component {
     super(props);
     this.state = {
       data: [],
+      id: '',
+      name: '',
+      vsible: false
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount = () => {
@@ -32,6 +39,42 @@ export default class ViewData extends React.Component {
       return;
     }
   }
+  openModal = () => {
+    this.setState({
+      visible : true
+    });
+  }
+
+  closeModal= () => {
+    this.setState({
+      visible : false
+    });
+  }
+  
+  editData = (item) => {
+    this.setState({
+      id: item.id,
+      name: item.name,
+    });
+    this.openModal();
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      name: this.state.name,
+    }
+    axios.put(`https://localhost:3000/api/data/${this.state.id}`, data).then(res => {
+      this.getData();
+    });
+    this.closeModal();
+  }
 
   render() {
     return (
@@ -53,7 +96,7 @@ export default class ViewData extends React.Component {
                   <td>{item.name}</td>
                   <td>{item.registerAt}</td>
                   <td>
-                    <button className="app-btn">Edit</button>
+                    <button className="app-btn" onClick={() => this.editData(item)}>Edit</button>
                     <button className="app-btn app-btn-danger" onClick={() => this.deleteData(item.id)}>Delete</button>
                   </td>
                 </tr>
@@ -61,6 +104,15 @@ export default class ViewData extends React.Component {
             })}
           </tbody>
         </table>
+        <Modal visible={this.state.visible} width="500" height="300" effect="fadeInUp" onClickAway={() => this.closeModal()}>
+          <div className="app-modal" style={{ background: '#282c34', width: '500px', height: '300px', borderRadius: '.25rem', padding: '1rem' }}>
+            <form onSubmit={this.handleSubmit}>
+            <input class="app-input" type="number" name="id" placeholder="ID" value={this.state.id} onChange={this.handleChange} style={{ textAlign: 'center', width: '120px' }} />
+            <input class="app-input" type="text" name="name" placeholder="Name" value={this.state.name} onChange={this.handleChange} />
+            <button className="app-btn" type="submit">Submit</button>
+          </form>
+          </div>
+        </Modal>
       </div>
     );
   }
