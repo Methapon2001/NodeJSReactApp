cccbcfff8422cf4ef825deeb07e467975371c4a1
Methapon2001 <dark.methapon13@gmail.com>
2022-02-25 16:30:48 +0700

    Updated query.

--- server/database/city.js
+++ server/database/city.js
@@ -1,6 +1,12 @@
 const connection = require('./connection');
 
 module.exports.get = async (request, response) => {
-    const cities = await connection.query('SELECT * FROM cities');
+    const { cityId } = request.params;
+    if (cityId) {
+        const city = await connection.query('SELECT * FROM cities WHERE id = ?', [cityId]);
+        return response.json(city[0]);
+    } else {
+        const cities = await connection.query('SELECT * FROM cities');
+    }
     return response.json(cities);
 }
